9da8ae29dbee199f74c6c133515156e428e0009f
Kalinote <840746219@qq.com>
2023-03-20 23:53:36 +0800

    完善了搜索功能

--- commands/functions/search.py
+++ commands/functions/search.py
@@ -5,8 +5,14 @@ from amiyabot import Chain, log
 from amiyabot import Message
 from playwright.async_api import async_playwright
 
+from ai.gpt.chatgpt import ChatGPT
 from configs import order_level, bot, PROXY_IP, PROXY_PORT
 
+def get_middle_chars(s, n):
+    middle = len(s) // 2
+    start = middle - n // 2
+    end = start + n
+    return s[start:end]
 
 class RequestWebsite:
     command = "#搜索"
@@ -21,7 +27,7 @@ async def search_website(data: Message):
     parser = argparse.ArgumentParser(prog=RequestWebsite.command, description=RequestWebsite.description)
 
     # 添加选项和参数
-    parser.add_argument('-k', '--keyword', type=str, default="什么是ChatGPT", help="需要搜索的关键词，默认为\"什么是ChatGPT\"")
+    parser.add_argument('-k', '--keyword', type=str, default="kalinote.top", help="需要搜索的关键词，默认为\"kalinote.top\"")
 
     # 在解析命令前先把-h处理掉，不然会导致程序退出
     parameters = shlex.split(data.text)[1:]
@@ -39,8 +45,7 @@ async def search_website(data: Message):
     keyword = args.keyword
     url = f"https://www.google.com/search?q={keyword}"
 
-    await bot.send_message(Chain().at(data.user_id).text("[实验功能]正在请求搜索内容，该过程需要6秒左右，请稍等..."), channel_id=data.channel_id)
-    # await bot.send_message(Chain().html(url, is_template=False, render_time=6000), channel_id=data.channel_id)
+    await bot.send_message(Chain().at(data.user_id).text("[实验功能]正在请求搜索内容，该过程需要数秒，请稍等..."), channel_id=data.channel_id)
 
     async with async_playwright() as p:
         browser = await p.chromium.launch(
@@ -52,8 +57,28 @@ async def search_website(data: Message):
         )
         page = await browser.new_page()
         await page.goto(url)
+
+        # 定位第一个 <h3> 标签的父标签并跳转
+        h3_element = await page.query_selector('h3')
+        parent_element = await h3_element.query_selector('xpath=..')
+        await page.goto(await parent_element.get_attribute('href'))
+
         screenshot_bytes = await page.screenshot(full_page=True)
         await bot.send_message(Chain().image(screenshot_bytes), channel_id=data.channel_id)
 
-    return
+        # content = await page.inner_text('h1, p, div')
+        content = await page.inner_text('body')
+
+    reply = ''
+    gpt = ChatGPT(temperature=0.3, system_order="你是一个网页内容分析助手，你需要根据用户发送的网页内容的消息来分析")
+
+    if len(content) > 3500:
+        reply += f'[由于ChatGPT API限制，只能识别3500个字符(4097 token)，但本篇文章全长共{len(content)}字符]\n[3500个字符是截取网页最中间的，最大程度减小顶部链接或底部链接对内容分析的影响]\n'
+        content = get_middle_chars(content, 3500)
+
+    # print(content)
+    gpt.add_conversation('user', content)
+    reply += gpt.call('请对以上内容进行分析，并使用中文描述')
+
+    return Chain(data).text(reply)
 
